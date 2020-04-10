0a926e76bf519435f7216d4f5a7540f085e84683
agoston <agoston@geogebra.org>
2020-04-10 18:04:42 +0200

    MOW-1005: backgrounding and foregrounding

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
@@ -7910,8 +7910,8 @@ public abstract class EuclidianController implements SpecialPointsListener {
 			app.getMaskWidgets().clearMasks();
 		}
 		for (Drawable dr : view.allDrawableList) {
-			if (dr instanceof DrawInlineText) {
-				((DrawInlineText) dr).toBackground();
+			if (dr instanceof DrawMedia) {
+				((DrawMedia) dr).toBackground();
 			}
 		}
 	}
